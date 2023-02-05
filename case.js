5990da21383cb918e6addd6ba966454f687b9705
rishadCancel <rishad.cancel@gmail.com>
2023-02-06 01:31:43 +0600

    first commit

---
+++ case.js
@@ -0,0 +1,10 @@
+const userName = "blackPink";
+const userInput = "blackPinK";
+
+if(userName.toLowerCase() === userInput)
+{
+    console.log("Valid");
+}
+else{
+    console.log("Invalid");
+}
---
+++ search.js
@@ -0,0 +1,4 @@
+const lyrics = "Tumi bondhu kala pakhi ami jeno ki. shada shada kala kala";
+
+const doesExist = lyrics.includes("ami");
+console.log(doesExist);
