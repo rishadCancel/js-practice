01da842c957cfeec098c7b7c68d0bf0accc47668
rishadCancel <rishad.cancel@gmail.com>
2023-02-06 01:33:49 +0600

    one change

--- search.js
+++ search.js
@@ -1,4 +1,4 @@
 const lyrics = "Tumi bondhu kala pakhi ami jeno ki. shada shada kala kala";
 
-const doesExist = lyrics.includes("ami");
+const doesExist = lyrics.includes("kil");
 console.log(doesExist);
